9d42ab1dbc85abbb2ba4f73f38895eeff12b5ae2
Leo Yanni <leo.yanni@post.runi.ac.il>
2025-11-05 21:28:16 +0200

    All assignments are completed

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,6 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int i=0;// Replace this comment with your code
 	}
 }
