8924cec3eb908c1a23fa8a5cb3196241210dd2ed
Leo Yanni <leo.yanni@post.runi.ac.il>
2025-11-06 14:21:15 +0200

    Completed Assignment

--- NumWords.java
+++ NumWords.java
@@ -6,7 +6,7 @@ public class NumWords {
 	   	int tens = (num /10) % 10;
 		int ones = (num %10);
 
-		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones");
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 
 
 
