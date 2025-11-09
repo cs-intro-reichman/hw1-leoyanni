c7bc06ffec7cc4fc8b2a77edf94ff095547eafc2
Elizaveta Khanan <154255907+Lizahanan@users.noreply.github.com>
2025-11-09 18:16:39 +0200

    Align code and improve comment in Bill3.java
    Aligned code and added comment for clarity.

--- Bill3.java
+++ Bill3.java
@@ -1,10 +1,14 @@
+/* Again same issue, make sure to align everything
+Also there should be clear separation between the part where 
+you extract your arguments and where "your" code starts */
+
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
 	String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int total = Integer.parseInt(args[3]);
+		int total = Integer.parseInt(args[3]); // here add a tab 
 		double share = Math.ceil (total / 3.0);
 
 		System.out.println("Dear " + name3 +  ", " + name2 + ", and " + name1 + 
